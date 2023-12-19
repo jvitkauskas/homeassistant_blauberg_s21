533c09d2c5911252bbdc1e571f16c7f80fd4e7a8
Julius Vitkauskas <zadintuvas@gmail.com>
2023-12-19 18:25:19 +0200

    Prepare for HACS

--- custom_components/blauberg_s21/config_flow.py
+++ custom_components/blauberg_s21/config_flow.py
@@ -43,6 +43,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Blauberg S21."""
 
     VERSION = 1
+    MINOR_VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
